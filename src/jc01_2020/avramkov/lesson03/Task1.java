69359618c3afc9b4c3de0d7b850f5b3e43df3112
avramkovk <60222602+avramkovk@users.noreply.github.com>
2020-01-27 16:16:26 +0300

    1 задача - тест пройден))

--- src/jc01_2020/avramkov/lesson03/Task1.java
+++ src/jc01_2020/avramkov/lesson03/Task1.java
@@ -16,14 +16,25 @@ public class Task1 {
 		Scanner scanner = new Scanner(System.in);
 		long input = scanner.nextLong();
 
-		int maxDigit = (int)(input % 10);
-		while (input > 0){
-			int currentDigit = (int)(input % 10);
-			if (currentDigit > maxDigit){
+		int maxDigit = (int) (input % 10);
+		if (input > 0) {
+			while (input > 0) {
+				int currentDigit = (int) (input % 10);
+				if (currentDigit > maxDigit) {
+					maxDigit = currentDigit;
+				}
+				input = input / 10;
+			}
+		} else if (input < 0) {
+			input = -1 * input;
+			while (input > 0) {
+			int currentDigit = (int) (input % 10);
+			if (currentDigit > maxDigit) {
 				maxDigit = currentDigit;
 			}
 			input = input / 10;
 		}
+	}
 		System.out.println(maxDigit);
 	}
 }
