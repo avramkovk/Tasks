cbd3f89a37792700a6c260d738cb3cfbb6f4c946
avramkovk <60222602+avramkovk@users.noreply.github.com>
2020-01-27 15:58:23 +0300

    1 задача

--- src/jc01_2020/lesson03/Task1.java
+++ src/jc01_2020/lesson03/Task1.java
@@ -16,6 +16,14 @@ public class Task1 {
 		Scanner scanner = new Scanner(System.in);
 		long input = scanner.nextLong();
 
-
+		int maxDigit = (int)(input % 10);
+		while (input > 0){
+			int currentDigit = (int)(input % 10);
+			if (currentDigit > maxDigit){
+				maxDigit = currentDigit;
+			}
+			input = input / 10;
+		}
+		System.out.println(maxDigit);
 	}
 }
