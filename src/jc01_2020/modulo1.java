8cb5828ef1b5aa9b76e23e44aeaba1c45a29c7ea
avramkovk <60222602+avramkovk@users.noreply.github.com>
2020-01-29 22:56:57 +0300

    lesson2 все тесты пройдены/ разобраться как работают исключения

--- src/jc01_2020/modulo1.java
+++ src/jc01_2020/modulo1.java
@@ -3,7 +3,7 @@ import java.lang.Math;
 public class modulo1 {
     public static void main(String[] args) {
         System.out.println("Остаток от деления отрицательного числа -15 на 4");
-        System.out.println(Math.floorMod(-15,4));
-        System.out.println(-15%4);
+        System.out.println(Math.floorMod(17,-5)); //ищет неполное частное
+        System.out.println(17%-5); //остаток от деления
     }
 }
