f97526791019c65e0654438f92e4a5cc8137e5f1
igor <gob1in@Mail.ru>
2020-01-30 19:11:04 +0300

    6

--- src/jc01_2020/modulo1.java
+++ src/jc01_2020/modulo1.java
@@ -4,6 +4,6 @@ public class modulo1 {
     public static void main(String[] args) {
         System.out.println("Остаток от деления отрицательного числа -15 на 4");
         System.out.println(Math.floorMod(17,-5)); //ищет неполное частное
-        System.out.println(17%-5); //остаток от деления
+        System.out.println(17%-5); //остаток от деления0
     }
 }
