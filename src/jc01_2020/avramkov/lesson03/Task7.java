5560593316f5bb7b9973f2cc33c60447851a45d1
avramkovk <60222602+avramkovk@users.noreply.github.com>
2020-01-30 13:02:06 +0300

    lesson2 все

--- src/jc01_2020/avramkov/lesson03/Task7.java
+++ src/jc01_2020/avramkov/lesson03/Task7.java
@@ -14,5 +14,12 @@ public class Task7 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int length = scanner.nextInt();
+		for (int i = 1; i <= length; i++) {
+			for (int j = 1; j <= length; j++) {
+				if ((i == j)||(i==(length-j+1))) System.out.print('+');
+				else System.out.print(' ');
+			}
+			System.out.println();
+		}
 	}
 }
