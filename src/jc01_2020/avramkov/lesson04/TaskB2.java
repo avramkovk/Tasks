ba7385ec6b173984f1091959cf6180a7fe7babe5
avramkovk <60222602+avramkovk@users.noreply.github.com>
2020-02-03 14:48:28 +0300

    4-B2 test +

--- src/jc01_2020/avramkov/lesson04/TaskB2.java
+++ src/jc01_2020/avramkov/lesson04/TaskB2.java
@@ -15,13 +15,44 @@ package jc01_2020.avramkov.lesson04;
 import java.util.Scanner;
 
 public class TaskB2 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int[][] array = new int[4][3];
-		for (int i = 0; i < array[0].length; i++) {
-			for (int j = 0; j < array.length; j++) {
-				array[j][i] = scanner.nextInt();
-			}
-		}
-	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] array = new int[4][3];
+
+//ввод массива с клавиатуры
+        System.out.println("Введите данные массива: ");
+        for (int i = 0; i < array[0].length; i++) { //array[0] - длина вложенного массива
+            for (int j = 0; j < array.length; j++) {
+                array[j][i] = scanner.nextInt();
+            }
+        }
+
+//вывод элементов массива на экран
+        System.out.println("Вывод массива: ");
+        for (int i = 0; i < array[0].length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(array[j][i]);
+            }
+            System.out.println();
+        }
+
+//поиск минимального элемента массива
+        int minValue = array[0][0]; //минимальное значение массива, начальное значение равно элементу array[0][0]
+        for (int i = 0; i < array[0].length; i++) { //длина вложенного массива
+            for (int j = 0; j < array.length; j++) {
+                if (array[j][i] < minValue) {
+                    minValue = array[j][i];
+                }
+            }
+        }
+
+//вывод на экран идексов минимального значения массива
+        for (int i = 0; i < array[0].length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[j][i] == minValue) {
+                    System.out.println("строка: " + i + ", столбец: " + j);
+                }
+            }
+        }
+    }
 }
