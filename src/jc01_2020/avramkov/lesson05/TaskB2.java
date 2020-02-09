eb7c5e9977ebff59ba87ff4c38e7a68344328e3c
avramkovk <60222602+avramkovk@users.noreply.github.com>
2020-02-10 00:30:49 +0300

    lesson 5 tests +

--- src/jc01_2020/avramkov/lesson05/TaskB2.java
+++ src/jc01_2020/avramkov/lesson05/TaskB2.java
@@ -11,14 +11,22 @@ package jc01_2020.avramkov.lesson05;
  */
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TaskB2 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		String str = scanner.nextLine();
-		boolean result = false;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        boolean result = false;
 
+        String regexp = "^[A-Z]{1}[a-z]+[\\-]?[A-Z]{1}[a-z]+$|^[А-Я]{1}[а-я]+[\\-]?[А-Я]{1}[а-я]+$";
+        Pattern pattern = Pattern.compile(regexp);
+        Matcher matcher = pattern.matcher(str);
 
+        if (matcher.find()) {
+            result = true;
+        } else result = false;
 		System.out.println(result);
-	}
+    }
 }
