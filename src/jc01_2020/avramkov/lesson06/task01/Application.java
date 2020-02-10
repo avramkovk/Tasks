3bd22a8ccce5426d0539128338a76356aa0eb0b6
avramkovk <60222602+avramkovk@users.noreply.github.com>
2020-02-10 17:43:49 +0300

    lesson 6 1-2 +

--- src/jc01_2020/avramkov/lesson06/task01/Application.java
+++ src/jc01_2020/avramkov/lesson06/task01/Application.java
@@ -5,7 +5,9 @@ public class Application {
 
         Time time1 = new Time((byte)5,(byte)3,(byte)1);
         Time time2 = new Time((byte)7,(byte)8,(byte)3);
-        System.out.println(time1.getFullSeconds());
 
+        time1.secondToDisplay(); //вывод на экран количества секунд time1
+
+        System.out.println(time1.compareTime(time2)); //сравнение времени, вывод на экран большего
     }
 }
