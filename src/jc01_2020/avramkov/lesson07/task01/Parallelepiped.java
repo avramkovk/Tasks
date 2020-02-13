e6bd70c120d1f313da6c31590b8e096ae283d92b
avramkovk <60222602+avramkovk@users.noreply.github.com>
2020-02-13 17:43:46 +0300

    lesson 7 1 + lesson 7 2 - нужны алгоритмы проверки хода фигур

--- src/jc01_2020/avramkov/lesson07/task01/Parallelepiped.java
+++ src/jc01_2020/avramkov/lesson07/task01/Parallelepiped.java
@@ -2,8 +2,19 @@ package jc01_2020.avramkov.lesson07.task01;
 
 public class Parallelepiped extends Shape{
 
-	private double length;
-	private double width;
 	private double height;
+	private double width;
+	private double length;
 
+	public Parallelepiped(double height, double width, double length) {
+		this.height = height;
+		this.width = width;
+		this.length = length;
+		this.setVolume(getVolume());
+	}
+
+	@Override
+	public double getVolume() {
+		return height * width * length;
+	}
 }
