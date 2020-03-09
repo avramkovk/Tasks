e2ed700f6c3af36c4719689b9a561c5bd0c28b78
avramkovk <60222602+avramkovk@users.noreply.github.com>
2020-03-09 17:05:28 +0300

    12. 5+

--- src/jc01_2020/avramkov/lesson12/Task5.java
+++ src/jc01_2020/avramkov/lesson12/Task5.java
@@ -8,8 +8,37 @@ package jc01_2020.avramkov.lesson12;
  */
 
 public class Task5 {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        Cat first = new Cat("Рыжик", 5);
+        Cat second = new Cat("Пушок", 5);
 
-	}
+        LifeCompare lifeCompare = (x, y) -> {
 
+            if (x.getLifeCat() == y.getLifeCat()) {
+                return "Коты имеют одинаковое количество жизней";
+
+            } else if (x.getLifeCat() > y.getLifeCat()) {
+                return x.getNameCat();
+            }
+
+            return y.getNameCat();
+        };
+        System.out.println(lifeCompare.compareCats(first, second));
+
+    }
 }
+
+class Cat {
+    String nameCat;
+    int lifeCat;
+
+    public String getNameCat() { return nameCat; }
+    public int getLifeCat() { return lifeCat; }
+
+    public Cat(String nameCat, int lifeCat) {
+        this.nameCat = nameCat;
+        this.lifeCat = lifeCat;
+    }
+}
+
+interface LifeCompare { String compareCats(Cat first, Cat second);}
