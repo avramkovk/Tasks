6a4dd91bca5ac88824e00957c27304779be14f89
avramkovk <60222602+avramkovk@users.noreply.github.com>
2020-03-12 14:28:49 +0300

    13. 3+

--- src/jc01_2020/avramkov/lesson13/Task2.java
+++ src/jc01_2020/avramkov/lesson13/Task2.java
@@ -8,16 +8,31 @@ package jc01_2020.avramkov.lesson13;
  */
 
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.TimeZone;
 
 public class Task2 {
 
         public static void main(String[] args) {
-            ZonedDateTime zDate = ZonedDateTime.now();
-            System.out.println(zDate);
+            ZonedDateTime zdtAtlantic = ZonedDateTime.now(ZoneId.of("Atlantic/Azores"));
+            System.out.println(zdtAtlantic);
 
+            List<String> list = new ArrayList<>(ZoneId.getAvailableZoneIds()); //список всех зон
 
+            Random r = new Random();
+            int[] rand = new int[10]; //массив с 10 рандомными индексами из list
+            for (int i = 0; i < rand.length; i++) {
+                rand[i]=r.nextInt(list.size());
+            }
+
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i+1 +". " + ZonedDateTime.of(LocalDateTime.now(),ZoneId.of(list.get(rand[i]))));
+            }
         }
     }
--- src/jc01_2020/avramkov/lesson13/Task3.java
+++ src/jc01_2020/avramkov/lesson13/Task3.java
@@ -10,8 +10,88 @@ package jc01_2020.avramkov.lesson13;
  *
  */
 
-public class Task3 {
-	public static void main(String[] args) {
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
-	}
+public class Task3 {
+    private static Map<String, LocalDate> birthDayMap = new HashMap<>();
+    private static Month month = LocalDate.now().getMonth();
+
+    public static void main(String[] args) {
+
+        birthDayMap.put("Rob", LocalDate.of(2019, 1, 12));
+        birthDayMap.put("Bob", LocalDate.of(1980, 3, 15));
+        birthDayMap.put("Anna", LocalDate.of(2010, 3, 9));
+        birthDayMap.put("Elza", LocalDate.of(2001, 6, 30));
+        birthDayMap.put("Mary", LocalDate.of(2000, 12, 15));
+
+        addBirthDayToMap("Donald", LocalDate.of(1999, 12, 31));
+        System.out.println(birthDayMap);
+        System.out.println("--------------------------------------------------------------------");
+        allBirthDay(birthDayMap, LocalDate.of(2020, 3, 10));
+        System.out.println("--------------------------------------------------------------------");
+        birthDayName("Anna");
+        birthDayName("Rob");
+        birthDayName("Elza");
+    }
+
+    //добавление человека в Карту
+    public static void addBirthDayToMap(String name, LocalDate localDate) {
+        birthDayMap.put(name, localDate);
+    }
+
+    //вывод ближайших дней рождения относительно определенной даты
+    public static void allBirthDay(Map<String, LocalDate> map, LocalDate otherDate) {
+        System.out.println("Ближайшие дни рождения к дате " + otherDate);
+        for (Map.Entry<String, LocalDate> entry : map.entrySet()) {
+            int t = entry.getValue().getDayOfYear() - otherDate.getDayOfYear();
+            if (t > 0 && t < 30) {
+                System.out.println(entry.getKey() + ", осталось " + t + " дн.");
+            } else if ((t < 0 && t > -30)) {
+                System.out.println("День рождения " + entry.getKey() + " был " + Math.abs(t) + " дн. назад");
+            }
+        }
+    }
+
+    //вывод оставшихся дней до дня рождения определенного человека относительно текущей даты
+    public static void birthDayName(String name) {
+        for (Map.Entry<String, LocalDate> entry : birthDayMap.entrySet()) {
+            int daysDifference = entry.getValue().getDayOfYear() - LocalDate.now().getDayOfYear();
+            String nameCurrent = entry.getKey();
+            if (name.equals(nameCurrent) && daysDifference > 0) {
+                System.out.println("До дня рождения " + name + " осталось " + daysDifference + " дн.");
+            } else if (name.equals(nameCurrent) && daysDifference < 0) {
+                System.out.println("День рождения " + name + " был " + Math.abs(daysDifference) + " дн. назад");
+            }
+        }
+    }
+
 }
+
+//рабочий вариант 1
+/*	public static void allBirthDay(Map<String, LocalDate> map) {
+		System.out.println("Скоро день рождения отмечают: ");
+		Iterator<Map.Entry<String, LocalDate>> iterator = map.entrySet().iterator();
+		while (iterator.hasNext()) {
+			Map.Entry<String, LocalDate> entry = iterator.next();
+			int t = entry.getValue().getDayOfYear() - LocalDate.now().getDayOfYear();
+			if (t > 0 && t < 30) {
+				System.out.println(entry.getKey() + ", осталось " + t + " дн." );
+			}
+		}
+	}*/
+
+//рабочий вариант 2
+/*public static void allBirthDay(Map<String, LocalDate> map) {
+		System.out.println("В следующем месяце дни рождения отмечают: ");
+		Iterator<Map.Entry<String, LocalDate>> iterator = map.entrySet().iterator();
+		while (iterator.hasNext()) {
+			Map.Entry<String, LocalDate> entry = iterator.next();
+			if (LocalDate.now().plusMonths(1).getMonth() == (entry.getValue().getMonth())) {
+				System.out.println(entry.getKey() + ", " + entry.getValue().getDayOfMonth() + " " + entry.getValue().getMonth());
+			}
+		}
+	}*/
