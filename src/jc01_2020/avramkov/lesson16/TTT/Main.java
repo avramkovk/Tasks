fd7623d9f8c5c31ebb1910c9b93d6355be052c74
avramkovk <60222602+avramkovk@users.noreply.github.com>
2020-03-31 21:37:36 +0300

    TTT

---
+++ src/jc01_2020/avramkov/lesson16/TTT/Main.java
@@ -0,0 +1,14 @@
+package jc01_2020.avramkov.lesson16.TTT;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Player player = new Player();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        executorService.execute(player);
+    }
+}
---
+++ src/jc01_2020/avramkov/lesson16/TTT/Player.java
@@ -0,0 +1,23 @@
+package jc01_2020.avramkov.lesson16.TTT;
+
+import java.util.concurrent.Semaphore;
+
+public class Player extends Thread {
+    private final Semaphore SEMAPHORE = new Semaphore(2, true);
+    Game game = new Game();
+
+    @Override
+    public void run() {
+
+        try {
+            SEMAPHORE.acquire();
+            game.buildTable();
+            game.movePlayer();
+            SEMAPHORE.release();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
+
